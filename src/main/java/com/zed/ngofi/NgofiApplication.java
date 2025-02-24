18f44f397dc3c19957af91c86719a2286c8d4ec4
Ssedie <zedric.rulloda@lorma.edu>
2025-02-24 08:55:35 +0800

    new na po ito

---
+++ src/main/java/com/zed/ngofi/NgofiApplication.java
@@ -0,0 +1,13 @@
+package com.zed.ngofi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgofiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgofiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zed/ngofi/NgofiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zed.ngofi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgofiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
